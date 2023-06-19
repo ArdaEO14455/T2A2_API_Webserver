8d03d78b8342b6af0e881e59a75f5350420a6b37
ArdaEO14455 <14455@coderacademy.edu.au>
2023-06-19 16:22:41 +1000

    created stock list model

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-Test
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://stock_taker:password@localhost:5432/stock'
+
+db = SQLAlchemy(app)
+
+class Stock_List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text())
+    company = db.Column(db.Text())
+    quantity = db.Column(db.Integer)
+
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print('Stock Tables Created Successfully')
+
+@app.route('/')
+def index():
+    return 'Hello, world'
+
+if __name__ == '__main__':
+    app.run(debug=True)
