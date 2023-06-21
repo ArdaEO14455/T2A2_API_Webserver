7d4eb334e0afed3bb06b1cf36770e091b7bb75df
ArdaEO14455 <14455@coderacademy.edu.au>
2023-06-21 14:45:18 +1000

    added models

--- blueprints/cli_bp.py
+++ blueprints/cli_bp.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from init import db
+from models.inventory import *
 
 
 cli_bp = Blueprint('db', __name__)
@@ -10,6 +11,52 @@ def create_db():
     db.create_all()
     print('Stock Tables Created Successfully')
 
-@cli_bp.cli.command('stock_list')
-def seed_db():
-    pass
+@cli_bp.cli.command('create_inventory')
+def create_inventory():
+    stock = [
+        Stock_Items(
+            item_name = 'Break Free',
+            company = 'Chain of Ponds',
+            quantity = 15,
+            item_type =  'Wine',
+            item_type_category = 'Shiraz',
+            unit = 'Bottle',
+            cost_price = 18
+        ),
+        Stock_Items(
+            item_name = 'Nectar of the Hops',
+            company = 'Willie the Boatman',
+            quantity = 4,
+            item_type =  'Beer',
+            item_type_category = 'New England IPA',
+            unit = 'Keg',
+            cost_price = 400
+        ),
+        Stock_Items(
+            item_name = 'Pash The Magic Dragon',
+            company = 'Batch Brewing Company',
+            quantity = 40,
+            item_type =  'Beer',
+            item_type_category = 'Sour Beer',
+            unit = 'can',
+            cost_price = 5.80
+        )
+        
+    ]
+
+    db.session.query(Stock_Items).delete()
+    db.session.add_all(stock)
+    db.session.commit()
+
+    bar = [
+        Bar_Items(
+            
+        )
+
+    ]
+
+    db.session.query(Bar_Items).delete()
+    db.session.add_all(bar)
+    db.session.commit()
+
+    print("Inventory Created")
--- models/inventory.py
+++ models/inventory.py
@@ -4,6 +4,34 @@ from marshmallow.validate import Length, OneOf, And, Regexp, ValidationError
 
 class Stock_List(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
+    bar_id = db.Column(db.Integer, db.ForeignKey)
+
     name = db.Column(db.Text())
     company = db.Column(db.Text())
     quantity = db.Column(db.Integer)
+
+
+class Bar_Items(db.Model):
+    bar_id = db.Column(db.Integer, primary_key=True)
+
+    stock_id = db.Column(db.Integer, db.ForeignKey('stock_items.id', ondelete='CASCADE'), nullable=False)
+
+    item_name = db.Column(db.Text)
+    company = db.Column(db.Text)
+    item_type = db.Column(db.Text()) #Wine, Beer, Spirit, Liqueur, Soft Drink
+    item_type_category = db.Column(db.Text()) #Shiraz, IPA, Sauvignon Blanc
+    unit = db.Column(db.Category()) #bottle, can, keg, etc 
+    quantity = db.Column(db.Integer)
+    max_quantity = db.Column(db.Integer)
+    bar_price = db.Column(db.Integer)
+
+class Stock_Items(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.Text)
+    company = db.Column(db.Text())
+    quantity: db.Column(db.Integer)
+    item_type = db.Column(db.Text()) #Wine, Beer, Spirit, Liqueur, Soft Drink
+    item_type_category = db.Column(db.Text()) #Shiraz, IPA, Sauvignon Blanc
+    unit = db.Column(db.Category()) #bottle, can, keg, etc
+    cost_price = db.Column(db.Integer)
